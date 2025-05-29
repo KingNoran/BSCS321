2da091322ddcaa13f6932c1bce9e5498300a79b7
KingNoran <kenreyes1234567@gmail.com>
2025-05-29 08:35:28 +0800

    added Symbols and SymbolTables!

--- ProgrammingLanguages/Prelims/PerformanceTask.py
+++ ProgrammingLanguages/Prelims/PerformanceTask.py
@@ -3,6 +3,9 @@
 # EOF (end-of-file) token is used to indicate that 
 # there is no more input left for lexical analysis
 
+from collections import OrderedDict
+
+
 INTEGER             = 'INTEGER'
 REAL                = 'REAL'
 INTEGER_CONST       = 'INTEGER_CONST'
@@ -25,6 +28,56 @@ VAR                 = 'VAR'
 COLON               = 'COLON'
 COMMA               = 'COMMA'
 EOF                 = 'EOF'
+
+
+
+class Symbol(object):
+    def __init__(self, name, type=None):
+        self.name = name
+        self.type = type
+        
+class BuiltinTypeSymbol(Symbol):
+    def __init__(self,name):
+        super().__init__(name)
+        
+    def __str__(self):
+        return self.name
+    
+    __repr__ = __str__
+
+class VarSymbol(Symbol):
+    def __init__(self, name, type):
+        super().__init__(name, type)
+        
+    def __str__(self):
+        return f"{self.name}:{self.type}"
+    
+    __repr__ = __str__
+    
+class SymbolTable(object):
+    def __init__(self):
+        self._symbols = OrderedDict()
+        self._init_builtins()
+        
+    def _init_builtins(self):
+        self.define(BuiltinTypeSymbol('INTEGER'))
+        self.define(BuiltinTypeSymbol('REAL'))
+        
+    def __str__(self):
+        s = f"Symbols: {[value for value in self._symbols.values()]}"
+        return s
+    
+    __repr__ = __str__
+    
+    def define(self, symbol):
+        print('Define : %s' % symbol)
+        self._symbols[symbol.name] = symbol
+        
+    def lookup(self, name):
+        print('Lookup: %s' % name)
+        symbol = self._symbols.get(name)
+        # 'symbol' is either an instance of the Symbol class or 'None'
+        return symbol
 
 
 
@@ -546,6 +599,57 @@ class NodeVisitor(object):
         pass
 
 
+class SymbolTableBuilder(NodeVisitor):
+    def __init__(self):
+        self.symtab = SymbolTable()
+        
+    def visit_Block(self, node):
+        for declaraction in node.declarations:
+            self.visit(declaraction)
+        self.visit(node.compound_statement)
+        
+    def visit_Program(self, node):
+        self.visit(node.block)
+        
+    def visit_BinOp(self, node):
+        self.visit(node.left)
+        self.visit(node.right)
+        
+    def visit_Num(self, node):
+        pass
+    
+    def visit_UnaryOp(self, node):
+        self.visit(node.expr)
+        
+    def visit_Compound(self, node):
+        for child in node.children:
+            self.visit(child)
+            
+    def visit_NoOp(self, node):
+        pass
+    
+    def visit_VarDecl(self, node):
+        type_name = node.type_node.value
+        type_symbol = self.symtab.lookup(type_name)
+        var_name = node.var_node.value
+        var_symbol = VarSymbol(var_name, type_symbol)
+        self.symtab.define(var_symbol)
+        
+    def visit_Assign(self, node):
+        var_name = node.left.value
+        var_symbol = self.symtab.lookup(var_name)
+        if var_symbol is None:
+            raise NameError(repr(var_name))
+        
+        self.visit(node.right)
+        
+    def visit_Var(self, node):
+        var_name = node.value
+        var_symbol = self.symtab.lookup(var_name)
+        
+        if var_symbol is None:
+            raise NameError(repr(var_name))
+        
 class Interpreter(NodeVisitor):
     GLOBAL_SCOPE = {}
     
